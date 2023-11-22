ed31ab5bde40772074e0138b1570081d62474175
fdbarnabe <fbarnabe@outlook.com>
2023-11-22 20:39:02 -0300

    react

--- src/App.js
+++ src/App.js
@@ -5,30 +5,3 @@ const App = () => {
     const multNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const [buttoms, setbuttoms] = useState ([])
 
-    useEffect (() => {
-        let Botoes = [];
-        for (let i = 1; i<=10; i++){    
-            if(i%3 == 0){
-                Botoes.push(<><button onClick={() => setNumber (i)}>{i}</button><br/></>)
-            }
-            else{
-                Botoes.push(<><button onClick={() => setNumber (i)}>{i}</button></>)
-            }
-        }  
-        setbuttoms(Botoes)
-    }, [])
-
-    return <div>
-        {buttoms}
-
-    {/* <input
-        value={number}
-        onChange={(event) => setNumber(event.target.value)}
-        placeholder="Digite um número"></input> */}
-
-   {/*  {gerarBotoes()} */}
-    {number && multNumbers.map(item => <div>{item} X {number} = {item * number}</div>)}
-</div>
-}
-
-export default App;
