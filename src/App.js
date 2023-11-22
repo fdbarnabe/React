09686126e02b8a82b05c6ba6a675ed8de1f958b9
fdbarnabe <fbarnabe@outlook.com>
2023-11-22 20:51:02 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-    const [number, setNumber] = useState(2);
+    const [number, setNumber] = useState();
     const multNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const [buttoms, setbuttoms] = useState ([])
 
+
+    return <div>
+        {multNumbers.map((item) => {
+            if (item % 3 == 0) {
+                return <><button onClick={() => setNumber(item)}>{item}</button><br /></>
+            }
+            return <><button onClick={() => setNumber(item)}>{item}</button></>
+        })}
+
+
+        {number && multNumbers.map(item => <div>{item} X {number} = {item * number}</div>)}
+    </div>
+}
+
+export default App;
